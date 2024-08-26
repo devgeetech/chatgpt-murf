f3432c1ca58fd49e96d40da9af6c3271fcb89b20
joel.gee.roy <joel.gee.roy@murf.ai>
2024-08-26 23:22:49 +0530

    audio input

--- src/components/GptForm/index.tsx
+++ src/components/GptForm/index.tsx
@@ -1,6 +1,11 @@
 import { useState, useRef } from "react";
-import { generateChatGPTResponse, ChatGPTMessage } from "../../api/OpenAiAPI";
+import {
+  generateChatGPTResponse,
+  generateTranscription,
+  ChatGPTMessage,
+} from "../../api/OpenAiAPI";
 import { generateSpeechWithKey, GenerateSpeechInput } from "../../api/MurfAPI";
+import { useRecorder } from "../../util/recorder";
 import GPTQueryItem from "../GPTQueryItem";
 import Spinner from "../Spinner";
 
@@ -17,11 +22,16 @@ type GPTQuery = {
 
 function GPTForm() {
   const [prompt, setPrompt] = useState("");
-  const [status, setStatus] = useState(STATUS_TYPES.IDLE);
+  const [status, setStatus] = useState<STATUS_TYPES>(STATUS_TYPES.IDLE);
+  const [recordingStatus, setRecordingStatus] = useState<STATUS_TYPES>(
+    STATUS_TYPES.IDLE
+  );
   const [gptQueries, setGptQueries] = useState([] as GPTQuery[]);
   const [currentAudioId, setCurrentAudioId] = useState(
     undefined as number | undefined
   );
+
+  const { startRecording, stopRecording, audioFile, recording } = useRecorder();
 
   const audioElementRef = useRef<HTMLAudioElement>(null);
 
@@ -29,12 +39,21 @@ function GPTForm() {
     setPrompt(e.target.value);
   }
 
-  async function handlePromptSubmit() {
-    if (!prompt || prompt.trim() === "") return;
+  async function handlePromptSubmit(customPrompt?: string) {
+    const finalPrompt = customPrompt || prompt;
+    if (!finalPrompt || finalPrompt.trim() === "") {
+      console.debug("Prompt is empty");
+      return;
+    }
 
     setStatus(STATUS_TYPES.LOADING);
 
-    const messagesList = [] as ChatGPTMessage[];
+    const messagesList = [
+      {
+        role: "system",
+        content: "You are a helpful assistant",
+      },
+    ] as ChatGPTMessage[];
     gptQueries.forEach((query) => {
       messagesList.push(
         {
@@ -49,7 +68,7 @@ function GPTForm() {
     });
     messagesList.push({
       role: "user",
-      content: prompt,
+      content: finalPrompt,
     });
 
     const gptRes = await generateChatGPTResponse({ messages: messagesList });
@@ -57,7 +76,12 @@ function GPTForm() {
     const murfPayload: GenerateSpeechInput = {
       voiceId: "en-US-marcus",
       format: "MP3",
-      text: gptRes.data.choices[0].message?.content as string,
+      style: "Conversational",
+      text: gptRes.choices[0].message?.content as string,
+      pronunciationDictionary: {
+        // "2010": { type: "SAY_AS", pronunciation: "two thousand ten" },
+      },
+      modelVersion: "GEN2",
     };
 
     const murfAudio = await generateSpeechWithKey(murfPayload);
@@ -65,8 +89,8 @@ function GPTForm() {
     setGptQueries([
       ...gptQueries,
       {
-        question: prompt,
-        gptResponse: gptRes.data.choices[0].message?.content as string,
+        question: finalPrompt,
+        gptResponse: gptRes.choices[0].message?.content as string,
         gptResponseAudioUrl: murfAudio.data.audioFile,
       },
     ]);
@@ -74,6 +98,7 @@ function GPTForm() {
     setPrompt("");
 
     setStatus(STATUS_TYPES.IDLE);
+    handleAudioPlay(gptQueries.length);
   }
 
   function handleAudioPlay(audioIdToPlay: number) {
@@ -90,9 +115,27 @@ function GPTForm() {
     setCurrentAudioId(undefined);
   }
 
+  async function handleAudioRecording() {
+    if (!recording) {
+      startRecording();
+      setRecordingStatus(STATUS_TYPES.LOADING);
+    } else {
+      const aFile = await stopRecording();
+      setRecordingStatus(STATUS_TYPES.IDLE);
+      setStatus(STATUS_TYPES.LOADING);
+      console.log(aFile);
+      const transcript = await generateTranscription({
+        audioFile: aFile as File,
+        language: "en",
+      });
+      setPrompt(transcript.text);
+      handlePromptSubmit(transcript.text);
+    }
+  }
+
   return (
     <div className="gpt-form">
-      <div className="gpt-form__heading">ChatGPT + Murf</div>
+      <div className="gpt-form__heading">Murf Assistant</div>
       <div className="gpt-form__queries">
         {gptQueries.length > 0 ? (
           gptQueries?.map((gptQuery, index) => {
@@ -120,7 +163,18 @@ function GPTForm() {
           className="gpt-form__user-input__actual"
         />
         <button
-          onClick={handlePromptSubmit}
+          onClick={handleAudioRecording}
+          disabled={status === STATUS_TYPES.LOADING}
+          className="gpt-form__user-input__button"
+        >
+          <span className="material-symbols-outlined gpt-form__user-input__button__icon">
+            {recordingStatus === STATUS_TYPES.LOADING
+              ? "radio_button_checked"
+              : "mic"}
+          </span>
+        </button>
+        <button
+          onClick={() => handlePromptSubmit()}
           disabled={status === STATUS_TYPES.LOADING}
           className="gpt-form__user-input__button"
         >
